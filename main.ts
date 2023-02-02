12e995671d1a208536e03595852e5aaa9fbd1c21
connero <88785126+conneroisu@users.noreply.github.com>
2023-02-02 11:26:41 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -38,10 +38,6 @@ export default class VimToggle extends Plugin {
 		});
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new VimToggleTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 	
 	onunload() {
